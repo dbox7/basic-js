b08f464333594ebf3d0642f4ad0060399c9a8c9b
Dmitry Korobka <dbox.insight@gmail.com>
2022-10-18 20:04:59 +0300

    feat: count-cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(M) {
+  return M.reduce((s, i) => s += i.reduce((sum, item) => sum += item == '^^' ? 1 : 0, 0), 0);
 }
 
 module.exports = {
